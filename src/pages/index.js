b970d49ddf38808c49179361c1356c0134beb39c
Dennis Basyrov <dennis.basyrov@gmail.com>
2020-04-26 16:31:35 -0400

    mixins functions

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,4 +2,8 @@ import React from "react"
 import "../../public/styles/main.scss"
 
 
-export default () => <h1>Hello Gatsby!</h1>
+export default () => (
+<div className='main'>
+  <h1 className='main__paragraph'>Hello Gatsby!!</h1>
+</div>
+)
